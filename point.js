24152fb87a79fa4db745c5772ecea2dcbb58a0af
Enrique Junchaya <enriquejh97@gmail.com>
2022-11-01 17:24:08 -0300

    adds point class

---
+++ point.js
@@ -0,0 +1,34 @@
+const POINT_COLOR = color(0, 0, 255);
+const POINT_DIAM = 10;
+
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    // Compares this point with another by x coordinate
+    less_than(other) {
+        return this.x != other.x ? this.x < other.x : this.y < other.y;
+    }
+    show() {
+        noStroke();
+        fill(POINT_COLOR);
+        ellipse(this.x, this.y, POINT_DIAM, POINT_DIAM);
+    }
+}
+
+
+// Two times the area of triangle abc
+function area2(a, b, c) {
+    va = createVector(a.x, a.y);
+    vb = createVector(a.x, a.y);
+    vc = createVector(c.x, c.y);
+    vb.sub(va);
+    vc.sub(va);
+    return vbase.cross(vc);
+}
+
+// Is point c at the left of the line ab?
+function left(a, b, c) {
+    return area2(a, b, c) > 0;
+}
