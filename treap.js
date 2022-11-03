556647f4f97335d3c1e91022958b17510b6361f5
Byakko97 <enriquejh97@gmail.com>
2022-11-02 21:38:32 -0300

    add treap basic methods

--- treap.js
+++ treap.js
@@ -1,14 +1,36 @@
-class Node {
-    constructor(value) {
-        this.value = value;
+class Treap {
+    constructor(key) {
+        this.key = key;
         this.priority = random();
         this.l = null;
         this.r = null;
     }
 }
 
-class Treap {
-    constructor() {
-        this.root = null;
+// Splits the treap t into two. One with all its keys less than x
+// and the other one with the rest
+function split(t, x) {
+    if(!t) return [null, null];
+    if(t.key.less_than(x) ) {
+        var right;
+        [t.r, right] = split(t.r, x);
+        return [t, right];
+    } else {
+        var left;
+        [left, t.l] = split(t.l, x);
+        return [left, t];
+    }
+}
+
+// Merges treaps left and right in a single one
+function merge(left, right) {
+    if(!left) return right;
+    if(!right) return left;
+    if(left.priority > right.priority) {
+        left.r = merge(left.r, right);
+        return left;
+    } else {
+        right.l = merge(left, right.l);
+        return right;
     }
 }
